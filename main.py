1de2e939accb6427492958d3e541c69806440f87
mahasrinivas05-pixel <mahasrinivas05@gmail.com>
2026-04-10 19:37:27 +0530

    UI upgraded

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
-import os 
-from google import genai 
-from dotenv import load_dotenv 
- 
-load_dotenv() 
- 
-def generate_ai_content(concept, keywords, title, description): 
- 
-client = genai.Client( 
-api_key=os.environ.get("GEMINI_API_KEY"), 
-) 
- 
-        prompt = f""" 
-       You are a YouTube SEO + Content Expert. 
- 
-        INPUT: 
-        Concept of Video: 
-        {concept} 
- 
-        Keywords: 
-       {keywords} 
- 
-       Current Title: 
-      {title}
-      Current Description: 
-      {description} 
- 
-      YOUR TASK: 
- 
-       Improve the content (DO NOT keyword stuff) 
- 
-       Generate 3 BETTER titles: - Length: 40–60 characters - Use keywords naturally - Add curiosity + hook 
- 
-        Generate 1 optimized description: - Length: 250–350 characters 
- 
-         STRICT FORMAT: 
- 
-          Title 1: ... 
-          Title 2: ... 
-          Title 3: ... 
- 
-           Description: 
-            ... 
- 
-            Hashtags: 
-            ... 
-            """ 
- 
-            response = client.models.generate_content( 
-model="gemini-2.5-flash", 
-  contents=prompt, 
-   ) 
- 
-             return response.text
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_ai_content(concept, keywords, title, description):
+
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    prompt = f"""
+You are a YouTube SEO + Content Expert.
+
+INPUT:
+Concept of Video:
+{concept}
+
+Keywords:
+{keywords}
+
+Current Title:
+{title}
+
+Current Description:
+{description}
+
+YOUR TASK:
+
+1. Improve the content (DO NOT keyword stuff)
+
+2. Generate 3 BETTER titles:
+- Length: 40–60 characters
+- Use keywords naturally
+- Add curiosity + hook
+
+3. Generate 1 optimized description:
+- Length: 250–350 characters
+
+STRICT FORMAT:
+
+Title 1: ...
+Title 2: ...
+Title 3: ...
+
+Description:
+...
+
+Hashtags:
+...
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
